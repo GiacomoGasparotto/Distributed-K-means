d7341c191ece63ab7f17e362cf18036be277f2ab
Edoardo D'Amore <edoardo.damore@studenti.unipd.it>
2025-09-08 10:04:14 +0000

    added src module with all custom functions. Added bash functionalities and workflow inside notebook

---
+++ src/utils.py
@@ -0,0 +1,24 @@
+import os
+
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+
+def sparkSetup(
+    appName: str       
+) -> SparkSession:
+    """
+    Returns a `SparkSession` properly configured.
+    """
+    env_path = "environment.tar.gz"
+    env_path = os.path.abspath(env_path)
+    conf = SparkConf() \
+        .set("spark.archives", f"{env_path}#environment") \
+        .set("spark.executor.memory", "4096m")
+
+    spark = SparkSession \
+        .builder \
+        .master("spark://master:7077") \
+        .appName(appName) \
+        .config(conf = conf) \
+        .getOrCreate()
+    return spark
